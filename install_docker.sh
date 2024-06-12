4f583d878c2d9be63d5a0d399341ec4903332085
Carl Angelo Nievarez <carlangelopamparonievarez@gmail.com>
2024-06-12 10:48:19 +0800

    Update install_docker.sh
    Add docker-compose, add user to docker group and enable docker server

--- install_docker.sh
+++ install_docker.sh
@@ -12,5 +12,14 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker and Docker Compose
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-buildx-plugin
+
+# Add current user to the docker group
+sudo usermod -aG docker $USER
+
+# Enable Docker service to start on boot
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
